dfd8456d70f9eda60703656923e278f4f5a820bd
Nadeesha <aselasgk+nadeesha@gmail.com>
2026-04-01 22:05:00 +0530

    fix

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.printn("Backend Validation Engine");
+        System.out.println("Backend Validation Engine");
     }
 }
