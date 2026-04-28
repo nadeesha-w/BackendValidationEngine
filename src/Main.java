88b914832238aade814a1de6eee3852602b09529
Nadeesha <aselasgk+nadeesha@gmail.com>
2026-04-28 20:50:00 +0530

    feat: add User model class for test data

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Backend Validation Engine");
         DatabaseManager.connect();
+
+        User sample = new User(1, "nimali_p", "ACTIVE");
+        System.out.println("Loaded test user: " + sample.getUsername());
     }
 }
