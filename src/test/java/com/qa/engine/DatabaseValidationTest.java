8a64335cb255d5139c24172dda7fd3ca58ba76f9
Nadeesha <aselasgk+nadeesha@gmail.com>
2026-05-12 20:40:00 +0530

    test: add a JUnit smoke test to confirm the runner works

---
+++ src/test/java/com/qa/engine/DatabaseValidationTest.java
@@ -0,0 +1,11 @@
+package com.qa.engine;
+
+import org.junit.jupiter.api.Test;
+
+public class DatabaseValidationTest {
+
+    @Test
+    public void runnerIsWorking() {
+        System.out.println("JUnit is running.");
+    }
+}
